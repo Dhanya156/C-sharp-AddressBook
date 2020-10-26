053f34515e1b5b89dfc9c26f5481d34b606744c2
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-26 18:36:35 +0530

    Added : Method To Add Person Details

--- Program.cs
+++ Program.cs
@@ -11,7 +11,31 @@ namespace AddressBook
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
+
             
+            
+        }
+
+        ///Creating a method AddPerson to add person details  
+        public static void AddPerson()
+        {
+            ///Creating a new Person object called person from the class PersonDetails
+            PersonDetails person = new PersonDetails();
+            Console.WriteLine("Enter Person FirstName");
+            person.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Person LastName");
+            person.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Person Address");
+            person.Address = Console.ReadLine();
+            Console.WriteLine("Enter Person City");
+            person.City = Console.ReadLine();
+            Console.WriteLine("Enter Person State");
+            person.State = Console.ReadLine();
+            Console.WriteLine("Enter ZipCode");
+            person.ZipCode = Console.ReadLine();
+            Console.WriteLine("Enter Person PhoneNUmber");
+            person.PhoneNumber = Console.ReadLine();
+            People.Add(person);
         }
     }
 }
