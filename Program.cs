0dc1b09a733cdfd5049235bc5ea7636b3bced439
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-26 16:32:06 +0530

    Added : PersonDetails Class To Create Person  Details

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBook
 {
-    class Program
+    public class Program
     {
+        /// This list is to hold list of person details        
+        public static List<PersonDetails> People = new List<PersonDetails>();
         /// <summary>Defines the entry point of the application.</summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book");
+            
         }
     }
 }
